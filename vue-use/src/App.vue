d173522ac398b08014996ef6d497b2404e886ad0
liyi <liyi@sinux.com>
2020-08-04 17:39:38 +0800

    change

--- vue-use/src/App.vue
+++ vue-use/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-main></app-main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppMain from './components/AppMain';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppMain
   }
 }
 </script>
